ebb3e7b760565d0c4c61070a32591cad49135b04
hsmulhim <79mulhim@gmail.com>
2023-08-31 14:11:29 +0300

    added final fixes

--- weather_app/lib/screens/search_screen.dart
+++ weather_app/lib/screens/search_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/componets/search_appbar_component.dart';
 import 'package:weather_app/componets/search_bar_component.dart';
+import 'package:weather_app/componets/weather_card_component.dart';
 import 'package:weather_app/constants/spacings.dart';
 import 'package:weather_app/extensions/screen_size.dart';
 
@@ -35,12 +36,13 @@ class _SearchScreenState extends State<SearchScreen> {
                   itemCount: 5,
                   itemBuilder: (context, indext) {
                     return WeatherCardComponent(
-                      bgColor: Colors.deepPurple,
+                      bgMColor: Colors.yellow[600],
                       cityName: 'London',
                       imgUrl:
                           'https://icon-library.com/images/sunny-weather-icon/sunny-weather-icon-13.jpg',
                       status: 'the weather is cloudy',
                       temp: '23',
+                      isDay: true,
                     );
                   }),
             ),
@@ -51,80 +53,4 @@ class _SearchScreenState extends State<SearchScreen> {
   }
 }
 
-class WeatherCardComponent extends StatelessWidget {
-  const WeatherCardComponent({
-    super.key,
-    required this.bgColor,
-    required this.cityName,
-    required this.status,
-    required this.temp,
-    required this.imgUrl,
-  });
-
-  final Color bgColor;
-  final String cityName;
-  final String status;
-  final String temp;
-  final String imgUrl;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      clipBehavior: Clip.antiAlias,
-      margin: EdgeInsets.all(8),
-      height: context.getHeight() * 0.18,
-      width: context.getWidth() - 50,
-      decoration: BoxDecoration(
-        color: bgColor,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 8),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    cityName,
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  kVSpace16,
-                  SizedBox(
-                    width: 180,
-                    child: Text(
-                      status,
-                      style: TextStyle(
-                          overflow: TextOverflow.ellipsis,
-                          color: Colors.white,
-                          fontSize: 20),
-                    ),
-                  ),
-                  kVSpace8,
-                  Expanded(
-                    child: Text(
-                      "${temp}°",
-                      style: TextStyle(color: Colors.white, fontSize: 50),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Spacer(),
-            Image.network(
-              imgUrl,
-              // "https://icon-library.com/images/sunny-weather-icon/sunny-weather-icon-13.jpg",
-              height: context.getHeight() * 0.17,
-              width: context.getWidth() * 0.4,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+// this is the weather card
